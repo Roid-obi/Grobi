6a516c82a439364bbf93bb36f783032286408db1
Roid-obi <roidrobih@gmail.com>
2026-03-29 19:50:53 +0700

    package: update version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import Navbar from "@/components/Navbar/Navbar";
-import StyledComponentsRegistry from "../lib/registry";
 import "../styles/global.css";
 import { Toaster } from "react-hot-toast";
+import StyledComponentsRegistry from "./lib/registry";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html>
       <body>
         <Navbar />
         <StyledComponentsRegistry>
